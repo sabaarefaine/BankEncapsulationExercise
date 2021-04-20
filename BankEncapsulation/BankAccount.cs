132a441d8018752d87d60d7e43d3a72515e6f6fa
Saba Arefaine <arefainesaba@gmail.com>
2021-04-19 17:48:35 -0700

    inital commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,80 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+        //A private field (similar to variable) of type double named balance with a value of 0
+        private double balance = 0;
+        //same for a password
+        private string password = "password";
+
+        
+        public bool VerificationProcess()
+        {
+            Console.WriteLine("What is your password?");
+            var answer = Console.ReadLine();
+
+            if (answer == password)
+            {
+                return true;
+            }
+            else
+                Console.WriteLine("Incorrect Password");
+
+            return false;
+        }
+        //Define a method named Deposit that will accept a double and store that value in the balance field
+        //Encapsulation in deposit method
+
+        public void Deposit()
+        {
+            var verify = VerificationProcess();
+            if (verify)
+            {
+                Console.WriteLine("How much do you want to deposit?");
+                var deposit = int.Parse(Console.ReadLine());
+                balance += deposit;
+                Console.WriteLine($"Your balance is now ${balance}.");
+            }
+        }
+
+        //Define a method named GetBalance that will return the amount stored in the balance field
+        public void GetBalance()
+        {
+            var verify = VerificationProcess();
+            if (verify)
+            {
+                Console.WriteLine(balance);
+            }
+            
+        }
+
+      
+
+        public void Withdraw()
+        {
+            var verify = VerificationProcess();
+
+            if (verify)
+            {
+                Console.WriteLine("How much do you want to withdraw?");
+                var withdraw = int.Parse(Console.ReadLine());
+
+                if (withdraw > balance)
+                {
+                    Console.WriteLine("Insufficient funds");
+                }
+                else
+                {
+                    balance -= withdraw;
+                    Console.WriteLine($"Your balance is now ${balance}");
+                }
+            }
+
+        }
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new instance of the BankAccount class
+            var newAccount = new BankAccount();
+
+            //Allow the user of the application to Deposit money (plus other things)
+            string userSelection;
+            do
+            {
+                Console.WriteLine("What would you like to do today?\n" +
+                    "Deposit \n" +
+                    "Withdraw \n" +
+                    "Check Balance \n" +
+                    "Exit");
+                userSelection = Console.ReadLine().ToLower();
+
+                //switch case to allow user to make atm selection
+                switch (userSelection)
+                {
+                    case ("deposit"):
+                        newAccount.Deposit();
+                        break;
+                    case ("withdraw"):
+                        newAccount.Withdraw();
+                        break;
+                    case ("check balance"):
+                        newAccount.GetBalance();
+                        break;
+                    case ("exit"):
+                        break;
+                    default:
+                        Console.WriteLine("Please try again");
+                        break;
+                }
+                } while (userSelection != "exit");
+
+
+            }
+
+        }
+
+}
